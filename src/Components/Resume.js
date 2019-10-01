21b941d186eb2fa6aa38c2145c3d33838682957b
Jordan Mullen <jordan.mullen@hotmail.com>
2019-10-01 12:26:16 -0600

    updating dependencies and resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,7 +5,7 @@ return(
   <div>
     <h1>Resume Page</h1>
     <hr/>
-    <iframe class="scribd_iframe_embed" title="Resume" src="https://www.scribd.com/embeds/406658981/content?start_page=1&view_mode=scroll&access_key=key-SA130JIth5PiMsKCdEaS&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_99673" width="100%" height="675px" frameborder="0"></iframe>
+    <iframe class="scribd_iframe_embed" title="Resume" src="https://www.scribd.com/embeds/428304837/content?start_page=1&view_mode=scroll&access_key=key-SA130JIth5PiMsKCdEaS&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_99673" width="100%" height="675px" frameborder="0"></iframe>
   </div>
 )}
 
